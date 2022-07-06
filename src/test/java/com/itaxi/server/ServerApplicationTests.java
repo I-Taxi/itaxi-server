e1d54d4859ba834e6cea5c4b7bc82e19b125e721
Doplin <soeunsense7@gmail.com>
2022-07-06 21:25:54 +0900

    feat[#9] : try to add swagger 3.0.0
    - build.gradle : swagger 관련 dependency 추가
    - server/config/SwaggerConfig.java : swagger config code 추가
    - server/ServerApplication : 에러 수정을 위한 @SpringBootAplication Annotation 추가
    
    ETC
    현재 IllegelState Exception Error가 나고 있는 상태로, ServerApplication.java에 있는 ServerApplication 함수가 load되지 않음.

--- src/test/java/com/itaxi/server/ServerApplicationTests.java
+++ src/test/java/com/itaxi/server/ServerApplicationTests.java
@@ -2,8 +2,10 @@ package com.itaxi.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+@WebAppConfiguration
 class ServerApplicationTests {
 
 	@Test
