8f97b050112c99d9b73d452fd1440fb67f603dad
Doplin <soeunsense7@gmail.com>
2022-07-08 11:49:58 +0900

    feat[#7] : add Notice related Exception
    --------- description ---------
    Message.java : add NOTICE_TITLE_EMPTY_EXCEPTION, NOTICE_CONTENT_EMPTY_EXCEPTION
    NoticeTitleEmptyException.java : create title empty exception
    NoticeContentEmptyException.java : create content empty exception

--- src/main/java/com/itaxi/server/exception/Message.java
+++ src/main/java/com/itaxi/server/exception/Message.java
@@ -5,4 +5,7 @@ public class Message {
     public static final String MEMBER_NOT_FOUND = "존재하지 않는 멤버입니다.";
     public static final String NOTICE_NOT_FOUND = "존재하지 않는 공지입니다.";
     public static final String POST_NOT_FOUND = "존재하지 않는 게시글입니다.";
+
+    public static final String NOTICE_TITLE_EMPTY_EXCEPTION = "공지사항의 제목이 없습니다";
+    public static final String NOTICE_CONTENT_EMPTY_EXCEPTION = "공지사항의 내용이 없습니다";
 }
---
+++ src/main/java/com/itaxi/server/exception/notice/NoticeTitleEmptyException.java
@@ -0,0 +1,10 @@
+package com.itaxi.server.exception.notice;
+
+import com.itaxi.server.exception.Message;
+import org.springframework.http.HttpStatus;
+
+public class NoticeTitleEmptyException extends NoticeException {
+    public NoticeTitleEmptyException() {
+        super(Message.NOTICE_TITLE_EMPTY_EXCEPTION, HttpStatus.BAD_REQUEST);
+    }
+}
