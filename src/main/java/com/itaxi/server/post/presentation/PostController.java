a0692740ed8b4ea72e78cf19fa1c7cbeb521c0fc
crystalYoo99 <crystal@handong.ac.kr>
2022-07-15 20:21:28 +0900

    feat[#6] : create post에 방만든사람 join하는 부분도 추가

--- src/main/java/com/itaxi/server/post/presentation/PostController.java
+++ src/main/java/com/itaxi/server/post/presentation/PostController.java
@@ -1,10 +1,7 @@
 package com.itaxi.server.post.presentation;
 
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
-
 import com.itaxi.server.docs.ApiDoc;
-import com.itaxi.server.place.application.PlaceDto;
 import com.itaxi.server.place.domain.repository.PlaceRepository;
 import com.itaxi.server.post.application.PostService;
 import com.itaxi.server.post.domain.repository.PostRepository;
@@ -12,12 +9,9 @@ import com.itaxi.server.post.domain.Post;
 import com.itaxi.server.place.domain.Place;
 import com.itaxi.server.post.application.PostDto;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-import com.itaxi.server.post.application.dto.JoinerCreateDto;
 import com.itaxi.server.post.application.dto.PostJoinDto;
-import com.itaxi.server.post.domain.Joiner;
 import com.itaxi.server.post.presentation.request.PostExitRequest;
 import com.itaxi.server.post.presentation.request.PostJoinRequest;
 import com.itaxi.server.post.presentation.response.PostInfoResponse;
@@ -37,33 +31,17 @@ public class PostController {
     private final PostRepository postRepository;
     private final PlaceRepository placeRepository;
 
-    /*@GetMapping
-    @ApiOperation(value = ApiDoc.TEST)
-    ResponseEntity<Object> aaa(){
-        System.out.println("aaaa");
-        return ResponseEntity.ok(null);
-    }*/
-
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/all", method = RequestMethod.GET)
     public Iterable<Post> findAllPost() {
         return postRepository.findAll();
     }
 
-    @RequestMapping(value = "/if", method = RequestMethod.GET)
-    //public PostDto.Res findPost(@RequestParam("dep")final long dep_id, @RequestParam("dst")final long dst_id, , @RequestBody final PostDto.GetPostReq dto) {
-    public Iterable<Post> getPost(@RequestParam("depId")final Long depId, @RequestParam("dstId")final Long dstId, @RequestParam("time")final LocalDateTime time) {
-        final Place departure = placeRepository.getById(depId);
-        final Place destination = placeRepository.getById(dstId);
-        return postRepository.findAllByDepartureAndDestinationAndDeptTime(departure, destination, time);
-        //return new PostDto.Res();
-    }
-
-    @RequestMapping(value = "/ifdto", method = RequestMethod.GET)
-    public List<PostDto.Res> getPostDto(@RequestParam("depId")final Long depId, @RequestParam("dstId")final Long dstId) {
+    @RequestMapping(method = RequestMethod.GET)
+    public List<PostDto.Res> getPostDto(@RequestParam("depId")final Long depId, @RequestParam("dstId")final Long dstId, @RequestParam("time")final LocalDateTime time) {
         final Long departureId = depId;
         final Place departure = placeRepository.getById(depId);
         final Place destination = placeRepository.getById(dstId);
-        List<PostDto.Res> resultList = (postRepository.findAllByDepartureAndDestination(departure, destination)).stream()
+        List<PostDto.Res> resultList = (postRepository.findAllByDepartureAndDestinationAndDeptTime(departure, destination, time)).stream()
                 .map(m -> new PostDto.Res(m))
                 .collect(Collectors.toList());
         return resultList;
@@ -71,11 +49,14 @@ public class PostController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
-    public PostDto.Res create(@RequestBody final PostDto.AddPostReq dto) {
+    public Post create(@RequestBody final PostDto.AddPostReq dto) {
         final Place departure = placeRepository.getById(dto.getDepId());
         final Place destination = placeRepository.getById(dto.getDstId());
         PostDto.AddPostPlaceReq postPlaceDto = new PostDto.AddPostPlaceReq(dto, departure, destination);
-        return new PostDto.Res(postService.create(postPlaceDto));
+        PostDto.Res result = new PostDto.Res(postService.create(postPlaceDto));
+        PostJoinDto joinDto= new PostJoinDto(dto.getUid(), dto.getStatus(), dto.getLuggage());
+        Post postInfo = postService.joinPost(result.getId(), joinDto);
+        return postInfo;
     }
 
     @PostMapping("/{postId}/join")
