e7cb4e6136feea24cfa83e87773926df5d9dfc9a
Doplin <soeunsense7@gmail.com>
2023-01-02 17:22:31 +0900

    feat[#79]: add Api Key Auth Filter for Security
    - ApiKeyAuthFilter.java : api filter to filtering servlet API Key
    - ApiKeyAuthToken.java : api token class

---
+++ src/main/java/com/itaxi/server/config/ApiKeyAuthToken.java
@@ -0,0 +1,29 @@
+package com.itaxi.server.config;
+
+import java.util.Collection;
+
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.Transient;
+
+@Transient
+public class ApiKeyAuthToken extends AbstractAuthenticationToken {
+
+    private String apiKey;
+
+    public ApiKeyAuthToken(String apiKey, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.apiKey = apiKey;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return apiKey;
+    }
+}
