3ceff1166f601d801a4e89216bce41d3cac109f5
Doplin <soeunsense7@gmail.com>
2023-01-13 15:37:55 +0900

    feat[#79] : add member not admin exception
    - add message in MEssage.java
    - add file MemberNotAdminException.java

--- src/main/java/com/itaxi/server/exception/member/MemberNotAdminException.java
+++ src/main/java/com/itaxi/server/exception/member/MemberNotAdminException.java
@@ -1,2 +1,10 @@
-package com.itaxi.server.exception.member;public class MemberNotAdminException {
+package com.itaxi.server.exception.member;
+
+import com.itaxi.server.exception.Message;
+import org.springframework.http.HttpStatus;
+
+public class MemberNotAdminException extends MemberException{
+    public MemberNotAdminException(HttpStatus httpStatus) {
+        super(Message.MEMBER_NOT_ADMIN, httpStatus);
+    }
 }
