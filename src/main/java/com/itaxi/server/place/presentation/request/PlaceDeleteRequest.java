2f070952ee6254b9207293245cafc871ad9cf5ac
Doplin <soeunsense7@gmail.com>
2023-01-16 14:59:38 +0900

    feat[#79] : add uid to Place CUD
    - add PlaceDeleteRequest.java
    - add admin checker to controller, service, UpdatePlaceDto, AddPlaceDto

--- src/main/java/com/itaxi/server/place/presentation/request/PlaceDeleteRequest.java
+++ src/main/java/com/itaxi/server/place/presentation/request/PlaceDeleteRequest.java
@@ -1,2 +1,12 @@
-package com.itaxi.server.place.presentation.request;public class PlaceDeleteRequest {
+package com.itaxi.server.place.presentation.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PlaceDeleteRequest {
+    private String uid;
 }
