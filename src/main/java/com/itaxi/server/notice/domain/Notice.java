d02d2254830a99824ae94a671d128d6a5269205e
daejun4582 <qkseowns9206@gmail.com>
2023-01-16 17:13:10 +0900

    [fix/#123] : 변수명 변경함

--- src/main/java/com/itaxi/server/notice/domain/Notice.java
+++ src/main/java/com/itaxi/server/notice/domain/Notice.java
@@ -34,7 +34,7 @@ public class Notice extends BaseEntity {
     private String content;
 
     @Column(nullable = false)
-    private int bannerType;
+    private int type;
     @Column(nullable = false)
     private LocalDateTime startTime;
 
@@ -46,7 +46,7 @@ public class Notice extends BaseEntity {
     public Notice(NoticeCreateDto noticeCreateDto) {
         this.title = noticeCreateDto.getTitle();
         this.content = noticeCreateDto.getContent();
-        this.bannerType = noticeCreateDto.getBannerType();
+        this.type = noticeCreateDto.getBannerType();
         this.startTime = noticeCreateDto.getStartTime();
         this.endTime = noticeCreateDto.getEndTime();
         this.setDeleted(false);
