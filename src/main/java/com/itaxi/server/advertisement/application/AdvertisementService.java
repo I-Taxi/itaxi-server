1a6b18f365c72170c2a13b6364ac4e19846e2029
Hyeongjin Kim <hhjj0506@gmail.com>
2023-02-10 15:01:46 +0900

    feat[#184]: advertisement GET APIs completed

--- src/main/java/com/itaxi/server/advertisement/application/AdvertisementService.java
+++ src/main/java/com/itaxi/server/advertisement/application/AdvertisementService.java
@@ -3,15 +3,21 @@ package com.itaxi.server.advertisement.application;
 import com.itaxi.server.advertisement.application.dto.AdvertisementUploadResponse;
 import com.itaxi.server.advertisement.domain.Advertisement;
 import com.itaxi.server.advertisement.domain.repository.AdvertisementRepository;
+import com.itaxi.server.exception.advertisement.ImageNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.zip.Deflater;
 import java.util.zip.Inflater;
@@ -22,33 +28,55 @@ public class AdvertisementService {
     @Autowired
     private final AdvertisementRepository advertisementRepository;
 
-    @Transactional
-    public AdvertisementUploadResponse createAdvertisement(MultipartFile file) throws IOException {
-        advertisementRepository.save(Advertisement.builder()
-                .name(file.getOriginalFilename())
-                .type(file.getContentType())
-                .image(compressImage(file.getBytes())).build());
+    private String FILE_PATH_ROOT = "/Users/david/Documents/server/src/main/java/com/itaxi/server/advertisement/images/";
 
-        return new AdvertisementUploadResponse(file.getOriginalFilename(), file.getContentType());
-    }
-
-    @Transactional
-    public Advertisement getAdvertisementInfo(String name) {
-        Optional<Advertisement> dbImage = advertisementRepository.findByName(name);
-
-        return Advertisement.builder()
-                .name(dbImage.get().getName())
-                .type(dbImage.get().getType())
-                .image(decompressImage(dbImage.get().getImage())).build();
-
-    }
+//    @Transactional
+//    public AdvertisementUploadResponse createAdvertisement(MultipartFile file) throws IOException {
+//        advertisementRepository.save(Advertisement.builder()
+//                .name(file.getOriginalFilename())
+//                .type(file.getContentType())
+//                .image(compressImage(file.getBytes())).build());
+//
+//        return new AdvertisementUploadResponse(file.getOriginalFilename(), file.getContentType());
+//    }
+//
+//    @Transactional
+//    public Advertisement getAdvertisementInfo(String name) {
+//        Optional<Advertisement> dbImage = advertisementRepository.findByName(name);
+//
+//        return Advertisement.builder()
+//                .name(dbImage.get().getName())
+//                .type(dbImage.get().getType())
+//                .image(decompressImage(dbImage.get().getImage())).build();
+//
+//    }
 
     @Transactional
     public byte[] getAdvertisement(String name) {
-        Optional<Advertisement> dbImage = advertisementRepository.findByName(name);
-        byte[] image = decompressImage(dbImage.get().getImage());
+        byte[] image;
+
+        try {
+            image = FileUtils.readFileToByteArray(new File(FILE_PATH_ROOT+name));
+        } catch (IOException e) {
+            throw new ImageNotFoundException();
+        }
+
         return image;
     }
+
+    @Transactional
+    public List<String> getAllAdvertisement(){
+        List<String> imageNames = new ArrayList<String>();
+        File filePath = new File(FILE_PATH_ROOT);
+        File[] files = filePath.listFiles();
+
+        for (File file : files) {
+            imageNames.add(file.getName());
+        }
+
+        return imageNames;
+    }
+
 
     public static byte[] compressImage(byte[] data) {
 
