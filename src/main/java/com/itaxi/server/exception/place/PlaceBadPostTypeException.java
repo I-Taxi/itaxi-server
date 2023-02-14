57fefa7898957975df1c6940543b2b474029dacc
YuSoeun <doplinblue@handong.ac.kr>
2023-02-15 05:07:50 +0900

    feat[#212]: 전체 검색 알고리즘 수정
    - postType 있는 경우
    - 한동대/양덕 전체가 모집 장소일 때 안 만들어지게

---
+++ src/main/java/com/itaxi/server/exception/place/PlaceBadPostTypeException.java
@@ -0,0 +1,8 @@
+package com.itaxi.server.exception.place;
+
+import com.itaxi.server.exception.Message;
+import org.springframework.http.HttpStatus;
+
+public class PlaceBadPostTypeException extends PlaceException {
+    public PlaceBadPostTypeException() {super(Message.PLACE_BAD_CNT, HttpStatus.BAD_REQUEST);}
+}
