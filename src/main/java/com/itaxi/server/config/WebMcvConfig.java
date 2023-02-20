152e2735ff0d2f6dcda83364a2362d1eaab089e6
daejun4582 <qkseowns9206@gmail.com>
2023-02-20 12:24:28 +0900

    [fix/#229] : XSS attack fillter create debug1 - swagger 충돌 해결

---
+++ src/main/java/com/itaxi/server/config/WebMcvConfig.java
@@ -0,0 +1,48 @@
+package com.itaxi.server.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itaxi.server.filters.HTMLCharacterEscapes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.List;
+@EnableWebMvc
+@Configuration
+public class WebMcvConfig {
+    @Bean
+    public WebMvcConfigurerAdapter controlTowerWebConfigurerAdapter() {
+        return new WebMvcConfigurerAdapter() {
+
+            @Override
+            public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+                super.configureMessageConverters(converters);
+
+                // 5. WebMvcConfigurerAdapter에 MessageConverter 추가
+                converters.add(htmlEscapingConveter());
+            }
+
+
+            private HttpMessageConverter<?> htmlEscapingConveter() {
+                ObjectMapper objectMapper = new ObjectMapper();
+                // 3. ObjectMapper에 특수 문자 처리 기능 적용
+                objectMapper.getFactory().setCharacterEscapes(new HTMLCharacterEscapes());
+
+                // 4. MessageConverter에 ObjectMapper 설정
+                MappingJackson2HttpMessageConverter htmlEscapingConverter =
+                        new MappingJackson2HttpMessageConverter();
+                htmlEscapingConverter.setObjectMapper(objectMapper);
+
+                return htmlEscapingConverter;
+            }
+        };
+    }
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+}
