c67e3c5ae23deaa1b4463703fb873ae4db1597df
Hyeongjin Kim <hhjj0506@gmail.com>
2023-02-22 14:48:22 +0900

    feat[#241]: advertisement getAdvertisementImage added

---
+++ src/main/java/com/itaxi/server/advertisement/presentation/request/AdGetImageRequest.java
@@ -0,0 +1,10 @@
+package com.itaxi.server.advertisement.presentation.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AdGetImageRequest {
+    private String imgName;
+}
--- src/main/java/com/itaxi/server/config/FilePathConfig.java
+++ src/main/java/com/itaxi/server/config/FilePathConfig.java
@@ -1,8 +1,6 @@
 package com.itaxi.server.config;
 
-import lombok.Getter;
-
 public class FilePathConfig {
 //    public static String FILE_PATH_ROOT = "~/image/";
-    public static String FILE_PATH_ROOT ="/Users/bandaejun/Desktop/image/";
+    public static String FILE_PATH_ROOT ="/";
 }
